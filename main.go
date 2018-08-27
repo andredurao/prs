6dee764b94f20e4068ce2be3345dd45008ad4537
André Durão <andreluisdurao@gmail.com>
2018-08-27 13:00:40 -0300

    Add gui pkg inspired on lazygit

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/andredurao/prs/pkg/gui"
 	_ "github.com/shurcooL/githubv4"
 	_ "golang.org/x/oauth2"
 )
 
 func main() {
 	fmt.Println("initializing...")
+	gui.NewGui()
 }
