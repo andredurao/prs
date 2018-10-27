143d56c5d35dffe2c88d6187a500cbfe15b3a8bf
André Durão <andreluisdurao@gmail.com>
2018-10-27 16:30:27 -0300

    Add graphql pull requests query

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -8,6 +8,20 @@ import (
 	"os"
 )
 
+type PullRequest struct {
+	URL         string
+	HeadRefName string
+	BaseRefName string
+}
+
+var query struct {
+	Search struct {
+		Nodes []struct {
+			PullRequest PullRequest `graphql:"... on PullRequest"`
+		}
+	} `graphql:"search(query: \"is:open is:pr repo:my_repo\", type: ISSUE, first: 90)"`
+}
+
 func githubToken() string {
 	return os.Getenv("GITHUB_AUTH_TOKEN")
 }
@@ -19,7 +33,7 @@ func PullRequests() {
 	httpClient := oauth2.NewClient(context.Background(), src)
 
 	client := githubv4.NewClient(httpClient)
-	err := client.Query(context.Background(), &base.Query, nil)
+	err := client.Query(context.Background(), &query, nil)
 	if err != nil {
 		log.Panicln(err)
 	}
